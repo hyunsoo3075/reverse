656227492f42134404ed1e0697b9b6e7749733fb
hyunsoo3075 <55557666+hyunsoo3075@users.noreply.github.com>
2021-10-31 16:38:41 -0400

    Added comments & main branch update

--- ReverseString.java
+++ ReverseString.java
@@ -1,34 +1,65 @@
 import java.util.*; 
 
 public class ReverseString{
-    public static void reverse(String str1){
-        char[] arr = str1.toCharArray();
+    /**
+    * Method that is called to reverse the inputted string
+    * should be only called if the input is 1 from scanner
+    * @param stringToReverse the string to be reversed inputted by the user 
+    */
+    public static void reverse(String stringToReverse){
+        char[] arr = stringToReverse.toCharArray();
+        int len = arr.length; 
+        int end = len - 1;
+        /**
+         * 1: algorithm: kept track of front and end pointers
+         * 2: swaps places
+         * 3: front incremented by 1, end decremented by 1
+         * note: it doesn't matter what the length of string is since my condition only swaps
+         * iff front is less than end (if length is odd number, front and end will be the same
+         * in the last loop where they meet together which does not satisfy the condition to swap)
+         */
         
-        int len = arr.length;
-        
-        
-        int tracker = len - 1;
-        for(int i = 0; i <= len/2; i++){
-            char holder = arr[i];
-            arr[i] = arr[tracker];
+        for(int front = 0; front < end; front++, end--){
+            char holder = arr[front];
+            arr[front] = arr[end];
             
-            arr[tracker] = holder;
-        
-            tracker--;
-
+            arr[end] = holder;
         }
         String tostr = String.valueOf(arr);
         System.out.println(tostr); 
+        
     }
+
+        /**
+         * Main to handle cases of the input
+         * @param args no argument needed from user
+         */
     public static void main(String[] args){
-        //System.out.print(typeof(args[0]));
-        if(args.length == 0){
-            System.out.print("please enter a argument");
-           
+        Scanner sc = new Scanner(System.in);
+        boolean valid = true;
+        /**
+         * endless loop until option 2 is inputted
+         * which then boolean variable "valid" is turned false
+         * and other inputs other than "1" and "2" is handled using default case
+         */
+        while(valid){
+            
+            System.out.println("Enter a number from the options below\n1: reverse a string\n2: quit");
+            switch(sc.next()){
+                case "1":
+                    System.out.println("Enter a string to reverse");
+                    reverse(sc.next());
+                    break;
+                case "2":
+                    valid = false;
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("invalid input\n");
+            }
+
         }
-        else{
-            reverse(args[0]);
-        }
+
         
     }
 }
